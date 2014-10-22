383153214669b1eaea634043b2e186d1757c7d0b
TC Juan <tcjuan@localhost.localdomain>
2014-10-22 09:53:35 +0800

    move subscrition tab to _menu

--- app/overrides/product_issue_admin_tab.rb
+++ app/overrides/product_issue_admin_tab.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_product_tabs",
                      :name => "add_issues_tab",
-                     :insert_bottom => "[data-hook='admin_product_tabs']",
+                     :insert_top => "[data-hook='admin_product_tabs'], #admin_product_tabs[data-hook]" ,
                      :partial => "spree/admin/products/issues",
                      :disabled => false)
